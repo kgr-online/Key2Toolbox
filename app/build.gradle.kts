d01e5d14c26fc63698d1832a754070485f41b294
kgr17 <27224290+kgr17@users.noreply.github.com>
2026-08-20 10:28:32 -0700

    Bump version to 4.8-beta7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 28
         targetSdk = 34
         versionCode = 1
-        versionName = "4.8-beta5"
+        versionName = "4.8-beta7"
     }
 
     // signingConfigs {
