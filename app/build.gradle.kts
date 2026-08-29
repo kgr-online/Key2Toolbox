cd4d89223f06476a4cedcb6761fb937a2d15f938
kgr17 <27224290+kgr17@users.noreply.github.com>
2026-08-29 16:07:34 -0700

    Bump version to 5.3.2 (36)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.kgr.key2toolbox"
         minSdk = 28
         targetSdk = 34
-        versionCode = 35
-        versionName = "5.3.1"
+        versionCode = 36
+        versionName = "5.3.2"
     }
 
     signingConfigs {
